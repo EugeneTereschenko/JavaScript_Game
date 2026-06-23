1c52f66a19f69a523e8b6667195b6032a98e6d53
yevhen <tereschenko.eugene@gmail.com>
2026-06-23 18:03:24 +0300

    add files

---
+++ game_copy/game.js
@@ -0,0 +1,88 @@
+import { InputHandler } from "./input.js";
+import { Player } from "./player.js";
+import { UI } from "./ui.js";
+import { Enemy, Angler1 } from "./enemy.js";
+
+export class Game {
+    constructor(width, height) {
+        this.width = width;
+        this.height = height;
+        this.keys = [];
+        this.enemies = [];
+        this.enemyTimer = 0;
+        this.enemyInterval = 1000;
+        this.ammo = 20;
+        this.maxAmmo = 50;
+        this.ammoTimer = 0;
+        this.ammoInterval = 500;
+        this.gameOver = false;
+        this.score = 0;
+        this.winningScore = 10;
+        this.gameTime = 0;
+        this.timeLimit = 5000;
+        this.ui = new UI(this);
+        this.player = new Player(this);
+        this.input = new InputHandler(this);
+    }
+    update(deltaTime) {
+        if (!this.gameOver) {
+            this.gameTime += deltaTime;
+        }
+        if (this.gameTime > this.timeLimit) {
+            this.gameOver = true;
+        }
+        this.player.update();
+        if (this.ammoTimer > this.ammoInterval){
+            if (this.ammo < this.maxAmmo) {
+                this.ammo++;
+                this.ammoTimer = 0;
+            } 
+        } else {
+                this.ammoTimer += deltaTime;
+        }
+        this.enemies.forEach(enemy => {
+            enemy.update();
+            if (this.checkCollision(this.player, enemy)){
+                enemy.markedForDeletion = true;
+            }
+            this.player.projectiles.forEach(projectile => {
+                if (this.checkCollision(projectile, enemy)){
+                    enemy.lives--;
+                    projectile.markedForDeletion = true;
+                    if (enemy.lives <= 0){
+                        enemy.markedForDeletion = true;
+                        if (!this.gameOver) {
+                            this.score += enemy.score;
+                        }
+                        if (this.score > this.winningScore) {
+                            this.gameOver = true;
+                        }
+                    }
+                }
+            })
+        });
+        this.enemies = this.enemies.filter(enemy => !enemy.markedForDeletion);
+        if (this.enemyTimer > this.enemyInterval && !this.gameOver){
+            this.addEnemy();
+            this.enemyTimer = 0;
+        } else {
+            this.enemyTimer += deltaTime;
+        }
+    }
+    draw(context) {
+        this.player.draw(context);
+        this.ui.draw(context);
+        this.enemies.forEach(enemy => {
+            enemy.draw(context);
+        });
+    }
+    addEnemy(){
+        this.enemies.push(new Angler1(this));
+    }
+    checkCollision(rect1, rect2){
+        return (rect1.x < rect2.x + rect2.width &&
+                rect1.x + rect1.width > rect2.x &&
+                rect1.y < rect2.y + rect2.height &&
+                rect1.height + rect1.y > rect2.y)
+    }
+}
---
+++ game_copy/player.js
@@ -0,0 +1,44 @@
+import { Projectile } from './projectile.js';
+
+export class Player {
+    constructor(game){
+        this.game = game;
+        this.projectiles = [];
+        this.width = 120;
+        this.height = 190;
+        this.x = 20;
+        this.y = 100;
+        this.speedY = 0;
+        this.maxSpeed = 5;
+    }
+    update(){
+        if (this.game.keys.includes('ArrowUp')) { 
+            this.speedY = -this.maxSpeed;
+        } else if (this.game.keys.includes('ArrowDown')){
+            this.speedY = this.maxSpeed;
+        } else {
+            this.speedY = 0;
+        }
+        this.y += this.speedY;
+
+        this.projectiles.forEach(projectile => {
+            projectile.update();
+        });
+        this.projectiles = this.projectiles.filter(projectile => !projectile.markForDeletion);
+    }
+    draw(context){
+        context.fillStyle = 'green';
+        context.fillRect(this.x, this.y, this.width, this.height);
+        this.projectiles.forEach(projectile => {
+            projectile.draw(context );
+        });
+    }
+    shootTop(){
+        if (this.game.ammo > 0){
+            this.projectiles.push(new Projectile(this.game, this.x + 80, this.y + 30));
+            this.game.ammo--;
+           // console.log(this.projectiles);
+        }
+
+    }
+}
